2e75cc7fe0bf3a22fcad6f8684ca74dcc07a94bd
Tushar Nair <tushar.nair@harrys.com>
2019-09-02 19:48:01 -0400

    Updating projects removing projects

--- fib-server/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    redisHost: process.env.REDIS_HOST,
-    redisPort: process.env.REDIS_PORT,
-    pgUsername: process.env.PGUSERNAME,
-    pgPassword: process.env.PGPASSWORD,
-    pgHost: process.env.PGHOST,
-    pgPort: process.env.PGPORT,
-    pgDatabase: process.env.PGDATABASE
-};
